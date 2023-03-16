ece62f30ae246f9ca5fdc37e77914eba5e193676
akram209 <akramhesham209@gmail.com>
2023-03-16 13:18:33 -0700

    5c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,16 @@
-
 #include <stdio.h>
+
 /**
- * main - Entry point 
+ * main - Entry point
  *
- * Description: A C program that print with puts functoin
+ * Description: A C program that prints with put functoin
  *
  * Return: Always 0 (success)
- */
+*/
 
-int main()
+int main(void)
 {
-   	puts ("\"Programming is like building a multilingual puzzle");
-  	 return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
 
