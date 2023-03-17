3ac88c06077d0a0151db1d53dcb74e120b391ca6
akram209 <akramhesham209@gmail.com>
2023-03-17 15:02:59 -0700

    aSASD

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch < 'z'; ch++)
+	putchar(ch);
+	return (0);
+}
