bf8d1644e685abcf7a73e312651892215eb6d2db
akram209 <akramhesham209@gmail.com>
2023-03-20 10:49:11 -0700

    1

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,22 +1,28 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * description: positive or  negative
+ *
+ * return 0 (success)
+*/
 
-/* betty style doc for function main goes there */
+
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
 	if (n > 0)
 	{
-	printf("%d is positive", n); }
+	printf("%i is positive", n); }
 	else if (n < 0)
 	{
-		printf("%d is negative ", n); }
+		printf("%i is negative ", n); }
 	else
-	{printf("%d is zero", n); }
+	{printf("%i is zero", n); }
 	return (0);
 }
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	putchar(ch);
+	for (ch = 'A'; ch <= 'Z'; ch++)
+	putchar(ch);
+	return (0);
+}
