4ee7e8ccdbb7b44ee68f4017a74d1dfe52230e40
akram209 <akramhesham209@gmail.com>
2023-03-24 17:18:57 -0700

    asd

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+include "main.h"
+
+/*
+ * mul - multiplies tow integers.
+ *
+ * @c :first input
+ * @b :second input 
+ *
+ * Return :result
+ *
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
