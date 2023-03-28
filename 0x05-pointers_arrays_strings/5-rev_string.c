9eb5ebbd4884134877fd96c5e60c8721393f6d06
akram209 <akramhesham209@gmail.com>
2023-03-27 17:21:48 -0700

    sad

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - print reversed string, followed by a new line.
+ * @s: pointer to the string to print
+ * Return: void
+*/
+
+void print_rev(char *s)
+{
+	int l, i;
+	char temp;
+
+	for (l = 0; s[l] != '\0'; ++l)
+
+	for (i = 0; i < l / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[l - 1 - i];
+		s[l - 1 - i] = temp;
+	}
+
+}
